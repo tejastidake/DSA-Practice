49fc8949778bd22d57f90d58d3422e68ef4a27cf
Tejas Vijay Tidake <100327660+tejastidake@users.noreply.github.com>
2024-01-31 07:36:13 +0530

    Solved: 709. To Lower Case.java
    Runtime: 0ms (Beats 100%) | Memory: 41.70MB (Top 26.34%)
    String

---
+++ String/709. To Lower Case.java
@@ -0,0 +1,16 @@
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+*/
+
+// Runtime: 0ms (Beats 100%) | Memory: 41.70MB (Top 26.34%)
+
+// String
+
+// Solution: https://leetcode.com/problems/to-lower-case/solutions/4651894/top-1-beats-100-in-0ms-single-line-answer-with-explanation-easy
+
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();         // method that converts string to lower case and returns
+    }
+}
